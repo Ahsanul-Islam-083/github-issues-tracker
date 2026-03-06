be2a9cf33407cf2c9806c4a6bb31f8f760731a56
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-03-06 23:32:22 +0600

    login functionalities added with demo credentials

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
+const userNameInput = document.getElementById("userNameInput");
+const passInput = document.getElementById("passInput");
+const loginBtn = document.getElementById("loginBtn");
 
+loginBtn.addEventListener("click", () => {
+    const userNameInputValue = userNameInput.value;
+    const passInputValue = passInput.value;
+    console.log(userNameInputValue, passInputValue);
+
+    if (userNameInputValue == "admin" && passInputValue == "admin123") {
+        alert('Login Success!');
+        window.location.assign("/home.html");
+    } else {
+        alert('Login Failed!');
+        return;
+    }
+
+})
